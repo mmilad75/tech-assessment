41b5bcdebe003213f42a1d03b8cbe60a0bb4fed5
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:21 +0200

    fix footer styles

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -2,22 +2,22 @@ import CopyRight from "../copy-right";
 import SocialMedia, { ISocialMediaProps } from "../social-media";
 
 export interface IFooterProps {
-  socialMedia: ISocialMediaProps[]
-};
+  socialMedia: ISocialMediaProps[];
+}
 
-const Footer: React.FC<IFooterProps> = ({socialMedia}) => {
+const Footer: React.FC<IFooterProps> = ({ socialMedia }) => {
   return (
-    <footer className="w-[1440px] h-[49px] px-10 flex">
-      <div className="flex justify-center items-center gap-3">
+    <footer className="container flex items-center py-2">
+      <div className="flex gap-3">
         {socialMedia.map((item, index) => (
           <SocialMedia {...item} key={index} />
         ))}
       </div>
-      <div className="flex w-[950px] px-5 py-3 justify-center items-center">
+      <div className="flex flex-grow justify-center items-center">
         <CopyRight />
       </div>
     </footer>
   );
-}
+};
 
 export default Footer;
