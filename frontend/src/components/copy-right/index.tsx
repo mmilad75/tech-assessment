15ebd1b9e47f1463d72b58ec42dde088de9f6430
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:21 +0200

    fix copyright gradient background

--- frontend/src/components/copy-right/index.tsx
+++ frontend/src/components/copy-right/index.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 
 const CopyRight: React.FC = () => {
   return (
-    <div className="flex w-fit justify-center items-center p-3 rounded-content bg-gradient-to-r from-primary-500/10  to-primary-500/5 border border-primary-500">
+    <div className="flex justify-center items-center p-3 rounded-content bg-brand-gradient">
       <span>Powered by</span>
-      <Image 
+      <Image
         className="w-[108px] h-[19px]"
-        src={require('../../assets/images/logo.png')}
+        src={require("../../assets/images/logo.png")}
         alt="absinthe"
       />
     </div>
-  )
+  );
 };
 
 export default CopyRight;
