afafb7c6f593c25e4e4ba7f1d5df92643191adc3
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:31 +0200

    add graphql

---
+++ frontend/src/services/graphql/apollo-client.ts
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client';
+import { BASE_URL } from './config';
+
+const httpLink = new HttpLink({
+  uri: BASE_URL,
+  headers: {
+    'x-hasura-admin-secret': process.env.HASURA_ADMIN_SECRET
+  }
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
---
+++ frontend/src/services/graphql/schema/query.ts
@@ -0,0 +1,15 @@
+import { gql } from "@apollo/client";
+
+export const GET_LOGS_QUERY = gql`
+  query MyQuery {
+    logs(limit: 10, order_by: {block_timestamp: desc}) {
+      address
+      block_number
+      block_timestamp
+      decoded
+      from
+      transaction_hash
+      to
+    }
+  }
+`;
