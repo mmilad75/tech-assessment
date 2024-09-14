8bee1dc9d5044c03a0d7ca63d656653bbda638ef
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:18 +0200

    add ProfileButton component

--- frontend/src/components/profile-button/index.tsx
+++ frontend/src/components/profile-button/index.tsx
@@ -1,6 +1,28 @@
-const ProfileButton = ({}) => {
+import Image from "next/image";
+import Link from "next/link";
+
+export interface IProfileButtonProps {
+  image: string;
+  text: string;
+  link?: string
+}
+
+const ProfileButton: React.FC<IProfileButtonProps> = ({
+  image, 
+  text, 
+  link = ''
+}) => {
   return (
-    <h1>Profile Button</h1>
+    <div >
+      <Link className="bg-elevation-3 w-fit flex px-4 py-[10px] rounded-xl" href={link}>
+        <Image 
+          className="w-[18px] h-[18px] object-cover rounded-full"
+          src={image}
+          alt={text}
+        />
+        <span className="font-semibold text-sm ml-3">{text}</span>
+      </Link>
+    </div>
   )
 }
 
---
+++ frontend/src/components/profile-button/profile-button.stories.ts
@@ -0,0 +1,16 @@
+import { Meta, StoryObj } from '@storybook/react';
+import ProfileButton, { IProfileButtonProps } from './index';
+
+export default {
+  title: 'Components/ProfileButton',
+  component: ProfileButton,
+} as Meta;
+
+type Story = StoryObj<IProfileButtonProps>;
+
+export const Default: Story = {
+  args: {
+    text: "bongo.eth" ,
+    image: require('../../assets/images/user.png')
+  }
+}
