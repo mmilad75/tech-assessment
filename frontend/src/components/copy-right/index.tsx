29dcdb99ee3ea9d306ce573307272b7ab71af4ec
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:23 +0200

    fix passing props from server components to client components issue

--- frontend/src/components/copy-right/index.tsx
+++ frontend/src/components/copy-right/index.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
+import Logo from "@/assets/images/logo.png";
 
 const CopyRight: React.FC = () => {
   return (
     <div className="flex justify-center items-center p-3 rounded-content bg-brand-gradient">
       <span>Powered by</span>
-      <Image
-        className="w-[108px] h-[19px]"
-        src={require("../../assets/images/logo.png")}
-        alt="absinthe"
-      />
+      <Image className="w-[108px] h-[19px]" src={Logo} alt="absinthe" />
     </div>
   );
 };
