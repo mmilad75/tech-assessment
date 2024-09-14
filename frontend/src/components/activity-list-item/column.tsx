7506e14c228d302be8d5bcae85848c2f90981eee
Milad <mmilad.sanati@gmail.com>
2024-09-14 23:57:22 +0200

    refactor columns component props

--- frontend/src/components/activity-list-item/column.tsx
+++ frontend/src/components/activity-list-item/column.tsx
@@ -2,16 +2,18 @@ import clsx from "clsx";
 import React from "react";
 
 export interface IColumnProps {
-  children: React.ReactNode,
-  className?: string
-};
+  className?: string;
+}
 
-const Column: React.FC<IColumnProps> = ({children, className}) => {
+const Column: React.FC<React.PropsWithChildren<IColumnProps>> = ({
+  children,
+  className,
+}) => {
   return (
     <div className={clsx("flex items-center px-6 py-5", className)}>
       {children}
     </div>
-  )
+  );
 };
 
 export default Column;
